65eeb20ca0881e87937271b093345a3123360d33
aimar01 <monre4500@gmail.com>
2024-08-14 10:26:48 +0200

    Añadido BackEnd

---
+++ ApiBasesDeDatosSolucion/ApiBasesDeDatosProyecto/IDentity/Serivicios/IUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiBasesDeDatosProyecto.IDentity.Serivicios
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<ApplicationUser>> GetAllUsersAsync();
+        Task<ApplicationUser?> GetUserByEmailAsync(string mail);
+
+        Task<bool> DeleteUserAsync(string userId);
+    }
+}
---
+++ ApiBasesDeDatosSolucion/ApiBasesDeDatosProyecto/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ApiBasesDeDatosProyecto.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Propiedades adicionales
+        public string? FullName { get; set; }
+        public string? Address { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Rol { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+    }
+}
