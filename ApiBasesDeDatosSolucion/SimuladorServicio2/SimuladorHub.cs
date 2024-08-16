33c8daf2ca90388f2218653eba64c29bef2559e5
rudakar <rubenberuete.1999@gmail.com>
2024-08-16 12:37:37 +0200

    #T2 Creados dos servicios que simulan a un sevidor al que nos conectamos como clientes conetandonos al hub y realizando una escucha en paralelo

---
+++ ApiBasesDeDatosSolucion/SimuladorServicio2/SimuladorHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+public class SimuladorHub : Hub
+{
+    public async Task EnviarMensaje(string mensaje)
+    {
+        await Clients.All.SendAsync("RecibirMensaje", mensaje);
+    }
+}
