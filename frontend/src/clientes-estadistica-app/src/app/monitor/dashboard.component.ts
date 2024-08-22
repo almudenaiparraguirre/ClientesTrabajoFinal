5c94a3e1d7c9e50624e02880022d25d1f984bc33
aimar01 <monre4500@gmail.com>
2024-08-22 12:46:41 +0200

    T6 - Modificados graficos estaticos para mostrar datos

--- frontend/src/clientes-estadistica-app/src/app/monitor/dashboard.component.ts
+++ frontend/src/clientes-estadistica-app/src/app/monitor/dashboard.component.ts
@@ -1,73 +1,141 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
 import { Chart } from 'chart.js/auto';
-import { MonitorDataService, MonitoringData } from 'src/app/servicios/monitor-data.service'; 
-import { UserService } from 'src/app/servicios/user.service'; 
-import { ClienteService } from 'src/app/servicios/cliente.service';
+import { MonitorDataService, MonitoringData } from 'src/app/servicios/monitor-data.service';
+import { UserService } from 'src/app/servicios/user.service';
 import { Subscription } from 'rxjs';
 import { Usuario } from '../clases/usuario';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements AfterViewInit {
-
-
+export class DashboardComponent implements AfterViewInit, OnDestroy {
   usuarios: Usuario[] = [];
-
   transferencias: MonitoringData[] = [];
   private subscription: Subscription = new Subscription();
+  private financialChart: Chart<'line', any, any>;
+  private countriesComparisonChart: Chart<'bar', any, any>;
+  private labels: string[] = [];
+  private data: number[] = [];
+  private receivingCountryLabels: string[] = [];
+  private receivingCountryData: number[] = [];
+  private sendingCountryLabels: string[] = [];
+  private sendingCountryData: number[] = [];
 
   constructor(private userService: UserService, private monitorDataService: MonitorDataService) { }
 
   ngOnInit(): void {
     this.loadUsuarios();
-    this.loadTransferencias();
+    this.loadTransferencias(); // Carga inicial de datos
   }
 
-  
   ngAfterViewInit(): void {
-    // Financial Chart
+    this.initFinancialChart(); // Inicializa el gráfico después de que la vista esté cargada
+    this.initCountriesComparisonChart();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+    if (this.financialChart) {
+      this.financialChart.destroy();
+    }
+    if (this.countriesComparisonChart) {
+      this.countriesComparisonChart.destroy();
+    }
+  }
+
+  private initFinancialChart() {
     const financialCtx = document.getElementById('financialChart') as HTMLCanvasElement;
-    new Chart(financialCtx, {
+
+    this.financialChart = new Chart(financialCtx, {
       type: 'line',
       data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+        labels: this.labels,
         datasets: [{
-          label: 'Earnings',
-          data: [1200, 1500, 1000, 2000, 1500, 1700, 1400, 1800, 2200, 2000],
+          label: 'Valor Transferencias',
+          data: this.data,
           borderColor: 'rgba(75, 192, 192, 1)',
           backgroundColor: 'rgba(75, 192, 192, 0.2)',
           fill: true
         }]
+      },
+      options: {
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Fecha'
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Valor ($)'
+            }
+          }
+        }
       }
     });
-
-    
-
-    // Subscriptions Chart
-    const subscriptionsCtx = document.getElementById('subscriptionsChart') as HTMLCanvasElement;
-    new Chart(subscriptionsCtx, {
-      type: 'line',
-      data: {
-        labels: ['12pm', '3pm', '6pm', '9pm', '12am', '3am', '6am', '9am'],
-        datasets: [{
-          label: 'Subscriptions',
-          data: [169, 180, 172, 181, 190, 178, 169, 162],
-          borderColor: 'rgba(255, 159, 64, 1)',
-          backgroundColor: 'rgba(255, 159, 64, 0.2)',
-          fill: true
-        }]
-      }
-    }); 
   }
 
-  loadTransferencias(): void {
+  private initCountriesComparisonChart() {
+    const countriesCtx = document.getElementById('countriesComparisonChart') as HTMLCanvasElement;
+
+    this.countriesComparisonChart = new Chart(countriesCtx, {
+      type: 'bar',
+      data: {
+        labels: this.receivingCountryLabels, // Usamos las mismas etiquetas para ambos conjuntos de datos
+        datasets: [
+          {
+            label: 'Transferencias Recibidas',
+            data: this.receivingCountryData,
+            backgroundColor: 'rgba(255, 159, 64, 0.2)',
+            borderColor: 'rgba(255, 159, 64, 1)',
+            borderWidth: 1,
+            barPercentage: 0.4, // Ajusta el ancho de las barras
+            categoryPercentage: 0.8
+          },
+          {
+            label: 'Transferencias Enviadas',
+            data: this.sendingCountryData,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            barPercentage: 0.4, // Ajusta el ancho de las barras
+            categoryPercentage: 0.8
+          }
+        ]
+      },
+      options: {
+        scales: {
+          x: {
+            stacked: false,
+            title: {
+              display: true,
+              text: 'País'
+            }
+          },
+          y: {
+            stacked: false,
+            title: {
+              display: true,
+              text: 'Número de Transferencias'
+            }
+          }
+        }
+      }
+    });
+  }
+
+  private loadTransferencias(): void {
     this.subscription.add(
       this.monitorDataService.getTransferencias().subscribe(
         data => {
           this.transferencias = data;
           this.updateTransferTable();
+          this.updateFinancialChart();
+          this.updateCountriesComparisonChart();
         },
         error => {
           console.error('Error fetching transferencias', error);
@@ -76,14 +144,13 @@ export class DashboardComponent implements AfterViewInit {
     );
   }
 
-  updateTransferTable(): void {
+  private updateTransferTable(): void {
     const tableBody = document.getElementById('transferTableBody');
     if (tableBody) {
-      tableBody.innerHTML = '';  // Limpia la tabla actual
+      tableBody.innerHTML = ''; // Limpia la tabla actual
       this.transferencias.forEach((transferencia) => {
         const row = document.createElement('tr');
         row.className = 'border-b';
-
         row.innerHTML = `
           <td class="px-4 py-2">${transferencia.name}</td>
           <td class="px-4 py-2">${transferencia.paisOrigen}</td>
@@ -98,8 +165,38 @@ export class DashboardComponent implements AfterViewInit {
     }
   }
 
+  private updateFinancialChart() {
+    this.labels = this.transferencias.map(t => new Date(t.timestamp).toLocaleTimeString());
+    this.data = this.transferencias.map(t => t.value);
 
-  loadUsuarios(): void {
+    this.financialChart.data.labels = this.labels;
+    this.financialChart.data.datasets[0].data = this.data;
+    this.financialChart.update();
+  }
+
+  private updateCountriesComparisonChart() {
+    const receivingCountryMap = this.transferencias.reduce((acc, t) => {
+      acc[t.paisDestino] = (acc[t.paisDestino] || 0) + 1;
+      return acc;
+    }, {} as { [key: string]: number });
+
+    const sendingCountryMap = this.transferencias.reduce((acc, t) => {
+      acc[t.paisOrigen] = (acc[t.paisOrigen] || 0) + 1;
+      return acc;
+    }, {} as { [key: string]: number });
+
+    const allCountries = new Set([...Object.keys(receivingCountryMap), ...Object.keys(sendingCountryMap)]);
+    this.receivingCountryLabels = Array.from(allCountries);
+    this.receivingCountryData = this.receivingCountryLabels.map(country => receivingCountryMap[country] || 0);
+    this.sendingCountryData = this.receivingCountryLabels.map(country => sendingCountryMap[country] || 0);
+
+    this.countriesComparisonChart.data.labels = this.receivingCountryLabels;
+    this.countriesComparisonChart.data.datasets[0].data = this.receivingCountryData;
+    this.countriesComparisonChart.data.datasets[1].data = this.sendingCountryData;
+    this.countriesComparisonChart.update();
+  }
+
+  private loadUsuarios(): void {
     this.subscription.add(
       this.userService.getUsuarios().subscribe(
         data => {
@@ -111,7 +208,5 @@ export class DashboardComponent implements AfterViewInit {
         }
       )
     );
-
-    
   }
 }
