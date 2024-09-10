bd0baeb507ff17c47f6abe33a004f2e724a4f03a
Sergio Melero <sergiomele97@gmail.com>
2024-09-10 12:22:19 +0200

    Cambiada url de Notify para longpolling adecuado

--- ApiBasesDeDatosSolucion/ApiBasesDeDatosProyecto/Repository/AccessMonitoringDataRepository.cs
+++ ApiBasesDeDatosSolucion/ApiBasesDeDatosProyecto/Repository/AccessMonitoringDataRepository.cs
@@ -62,7 +62,7 @@ namespace ApiBasesDeDatosProyecto.Repository
         private async Task NotifyChangesAsync()
         {
             // Asume que la API está alojada localmente; ajusta la URL según sea necesario
-            var notifyUrl = "https://localhost:7107/api/ClienteMonitoring/notify";
+            var notifyUrl = "https://backclientes-f4fjfwece9gta3c2.spaincentral-01.azurewebsites.net/api/monitoring/notify";
             var response = await _httpClient.PostAsync(notifyUrl, null);
 
             if (!response.IsSuccessStatusCode)
